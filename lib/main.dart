fe72eed19f79c9630dfdc65e4ba4ec8e7bfa98b5
Thomas <thomas.camlong@epitech.eu>
2020-02-14 12:37:22 +0100

    ios files, less useless files and less assets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(MaterialApp( debugShowCheckedModeBanner: false,  home: Passport(), ));
 
-class Home extends StatelessWidget {
+class Passport extends StatefulWidget {
+  @override
+  _PassportState createState() => _PassportState();
+}
+
+class _PassportState extends State<Passport> {
+  String name = "Thomas Camlong";
+  String username = "Ajnart";
+  String mail = "thomas.camlong@epitech.eu";
+  String number = "06 77 25 95 40";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My big application',
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.dark,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red[600],
-          title: Text('My App'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                // do something
-              },
-            )
-          ],
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {},
-              child: Text("Click me"),
-              color: Colors.red,
-              textColor: Colors.white,
-            ),
-            Text ("S'il vous plaît?"),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-          onPressed: null,
-          child: Icon(Icons.access_alarms), foregroundColor: Colors.white,
-        ),
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: [
+      //   BottomNavigationBarItem(icon: new Icon(Icons.access_alarms),title:Text("Test")),
+      //   BottomNavigationBarItem(title: Text("Test2"), icon: Icon(Icons.access_alarms))
+      //   ]),
+      appBar: AppBar(
+        title: Text("Appbar."),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SafeArea(
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/main.jpg"),
+                    radius: 50,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("NAME:",
+                    style: TextStyle(
+                        color: Colors.grey, letterSpacing: 2, fontSize: 15)),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("$name",
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 3,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("USERNAME:",
+                    style: TextStyle(
+                        color: Colors.grey, letterSpacing: 2, fontSize: 15)),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("$username",
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 3,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.email, color: Colors.grey[400]),
+                    FlatButton(
+                      onPressed: () {
+                        launch("mailto:$mail");
+                      },
+                      child: Text("$mail",
+                          style: TextStyle(
+                              color: Colors.amberAccent[200],
+                              letterSpacing: 1,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.grey[400]),
+                    FlatButton(
+                      onPressed: () {
+                        launch("tel://$number");
+                      },
+                      child: Text("$number",
+                          style: TextStyle(
+                              color: Colors.amberAccent[200],
+                              letterSpacing: 2,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                    )
+                  ],
+                ),
+              ],
+            )),
+      ),
+      endDrawer: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Text("Made with ♥ in Bordeaux",
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 1,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300)),
+        ],
       ),
     );
   }
